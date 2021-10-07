5eed272c4dcd5251fd2bec1cea2f12fb9a013543
Gilmar Vinicius Kich Bec <gilmarmscontato@gmail.com>
2021-10-07 14:54:04 -0300

    feat/ webhook logger

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const fs = require('fs');
+
+app.get('/list', (req, res) => {
+    fs.readdir('tmp/', (err, files) => {
+        return res.json(files)
+    });
+})
+
+app.get('/log/:id', (req, res) => {
+    fs.readFile(`tmp/${req.params.id}.log`, (err, data) => {
+        if (!data) {
+            return res.status(404).json({code: `ID[${req.params.id}] not exists`})
+        }
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
+})
+
+app.all('/add/*', (req, res) => {
+    const content = JSON.stringify({
+        url: req.url,
+        method: req.method,
+        headers: req.headers,
+        body: req.body
+    });
+
+    fs.appendFile(`tmp/${Math.floor(Date.now() / 1000)}.log`, content, err => {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+
+    return res.json('success')
+})
+
+app.all('/create/:id', (req, res) => {
+    const content = JSON.stringify({
+        url: req.url,
+        method: req.method,
+        headers: req.headers,
+        body: req.body
+    });
+
+    fs.appendFile(`tmp/${req.params.id}.log`, content, err => {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+
+    return res.json('success')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
